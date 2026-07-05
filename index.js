914f5ba4d0a6506be47c305562f46f07d7c7d165
Aditya2584 <bestrong2584@gmail.com>
2026-07-05 14:41:25 +0530

    Basic Express app with mongo

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const env = require('dotenv');
+const mongoose = require("mongoose");
+
+env.config();
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.get("/home", (req, res)=>{
+    console.log("Hitting /home")
+    return res.json({
+        success: true,
+        message: "Fetched home"
+    });
+})
+
+app.listen(process.env.PORT, async()=>{
+    console.log(`Server started on PORT ${process.env.PORT}`);
+
+    try{
+        await mongoose.connect(process.env.DB_URL);
+        console.log("Sucessfully connected to mongo");
+    }catch(err){
+        console.log("Not able to connect");
+    }
+   
+})
