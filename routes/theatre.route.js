5e21a2e50d0cb9113ea1d0ad45045225960a2429
Aditya2584 <bestrong2584@gmail.com>
2026-07-08 10:20:15 +0530

    Added get Theatre api

--- routes/theatre.route.js
+++ routes/theatre.route.js
@@ -3,6 +3,8 @@ const theatreMiddlewares = require("../middlewares/theatre.middleware")
 
 const routes = (app) =>{
     app.post('/mba/api/v1/theatres',theatreMiddlewares.validateTheatreCreateRequest , theatreController.create);
+
+    app.get('/mba/api/v1/theatres/:id', theatreController.getTheatre);
 }
 
 module.exports = routes;
