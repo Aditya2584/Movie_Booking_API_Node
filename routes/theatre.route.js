aa936e4b2de9de61f805b032d34e20528e9bdc85
Aditya2584 <bestrong2584@gmail.com>
2026-07-15 05:48:04 +0530

    Added a Authenticated middleware for checking API auth

--- routes/theatre.route.js
+++ routes/theatre.route.js
@@ -1,10 +1,11 @@
 const theatreController = require("../controllers/theatre.controller");
 const theatreMiddlewares = require("../middlewares/theatre.middleware")
+const authMiddleware = require("../middlewares/auth.middlewares");
 
 const routes = (app) =>{
     app.post('/mba/api/v1/theatres',theatreMiddlewares.validateTheatreCreateRequest , theatreController.create);
 
-    app.delete('/mba/api/v1/theatres/:id',theatreController.destroy);
+    app.delete('/mba/api/v1/theatres/:id',authMiddleware.isAuthenticated, theatreController.destroy);
 
     app.get('/mba/api/v1/theatres/:id', theatreController.getTheatre);
 
