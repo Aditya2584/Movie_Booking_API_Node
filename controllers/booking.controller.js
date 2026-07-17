1ea04701a95481db314e45280414cdf39b5c1a10
Aditya2584 <bestrong2584@gmail.com>
2026-07-17 22:28:26 +0530

    Added get Api for booking

--- controllers/booking.controller.js
+++ controllers/booking.controller.js
@@ -35,7 +35,50 @@ const update = async(req, res) =>{
     }
 }
 
+const getBookings = async(req, res, next) =>{
+    try{
+        const response = await bookingService.getBookings({userId: req.user});
+        successResponseBody.data = response;
+        successResponseBody.message = "Successfully fetch the booking";
+        return res.status(STATUS.OK).json(successResponseBody);
+    }catch(error){
+        errorResponseBody.err = error;
+        res.status(STATUS.INTERNAL_SERVER_ERROR).json(errorResponseBody);
+    }
+}
+
+const getAllBookings = async(req, res, next) =>{
+    try{
+        const response = await bookingService.getAllBookings();
+        successResponseBody.data = response;
+        successResponseBody.message = "Successfully fetch the booking";
+        return res.status(STATUS.OK).json(successResponseBody);
+    }catch(error){
+        errorResponseBody.err = error;
+        res.status(STATUS.INTERNAL_SERVER_ERROR).json(errorResponseBody);
+    }
+}
+
+const getBookingById = async(req, res, next) =>{
+    try{
+        const response = await bookingService.getBookingById(req.params.id, req.user);
+        successResponseBody.data = response;
+        successResponseBody.message = "Successfully fetched the booking";
+        return res.status(STATUS.OK).json(successResponseBody);
+    }catch(error){
+        if(error.err){
+            errorResponseBody.err = error.err;
+            return res.status(STATUS.OK).json(successResponseBody);  
+        }
+        errorResponseBody.err = error;
+        res.status(STATUS.INTERNAL_SERVER_ERROR).json(errorResponseBody);
+    }
+}
+
 module. exports = {
     create,
     update,
+    getBookings,
+    getAllBookings,
+    getBookingById,
 }
--- services/booking.service.js
+++ services/booking.service.js
@@ -44,7 +44,52 @@ const updateBooking = async(data, bookingId) => {
     }
 }
 
+const getBookings = async(data) =>{
+    try{
+        const response = await Booking.find(data);
+        return response;
+    }catch(error){
+        throw error;
+    }
+}
+
+const getAllBookings = async() =>{
+    try{
+        const response = await Booking.find();
+        // console.log(response);
+        return response;
+    }catch(error){
+        console.log(error)
+        throw error;
+    }
+}
+
+const getBookingById = async(id, userId) =>{
+    try{
+        const response = await Booking.findById(id);
+        if(!response){
+            throw {
+                err: "No booking records found for the id",
+                code: STATUS.NOT_FOUND
+            }
+        } 
+        if(response.userId != userId){
+            throw {
+                err: "Not able to access booking",
+                code: STATUS.UNAUTHORISED  
+            }
+        }
+        return response;
+    }catch(error){
+        console.log(error);
+        throw error;
+    }
+}
+
 module.exports = {
     createBooking,
     updateBooking,
+    getBookings,
+    getAllBookings,
+    getBookingById,
 }
