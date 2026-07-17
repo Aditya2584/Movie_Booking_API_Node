83ee2828a5fffe3555c7bdc848ffb382b7c9ae0c
Aditya2584 <bestrong2584@gmail.com>
2026-07-17 19:19:28 +0530

    Update booking Api done

--- middlewares/booking.middlewares.js
+++ middlewares/booking.middlewares.js
@@ -1,8 +1,9 @@
-const { STATUS } = require("../utils/constants")
+const { STATUS, USER_ROLE, BOOKING_STATUS } = require("../utils/constants")
 const { errorResponseBody, successResponseBody } = require("../utils/responsebody")
 const ObjectId = require("mongoose").Types.ObjectId;
 
-const theatreServices = require("../services/theatre.service")
+const theatreServices = require("../services/theatre.service");
+const userService = require("../services/user.service")
 
 const validateBookingCreateRequest = async(req, res, next) => {
     // theatre
@@ -55,6 +56,17 @@ const validateBookingCreateRequest = async(req, res, next) => {
     next();
 }
 
+const canChangeStatus = async(req, res, next) =>{
+    const user = await userService.getUserById(req.user);
+    if(user.userRole == USER_ROLE.customer && req.body.status && req.body.status != BOOKING_STATUS.cancelled){
+        errorResponseBody.err = "You are not allowed to change the booking status";
+        return res.status(STATUS.UNAUTHORISED).json(errorResponseBody);
+    }
+
+    next();
+}
+
 module.exports = {
     validateBookingCreateRequest,
+    canChangeStatus,
 }
